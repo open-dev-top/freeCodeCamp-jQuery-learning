ec132d16f454a6d4e6e9d0556edebd11622ab994
open dev <opendev.top@gmail.com>
2024-04-17 10:25:45 +0800

    Add id selector

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 /* Getting started with jQuery */
 
-$(document).ready(function () {
-  $("button").click(function () {
-    $("p").hide();
-  });
-});
-
 // $(function(){
 //     $("button").bind("click", function () {
 //         alert(1112);
 //       });
 // })
+
+$(document).ready(function () {
+  $("button").click(function () {
+    $("#cereal").hide();
+  });
+});
