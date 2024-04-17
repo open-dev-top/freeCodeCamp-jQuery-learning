6525526542493ba881c697a5d46f81cfe6371aa9
open dev <opendev.top@gmail.com>
2024-04-17 10:22:13 +0800

    Add example

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Getting started with jQuery */
+
+$(document).ready(function () {
+  $("button").click(function () {
+    $("p").hide();
+  });
+});
+
+// $(function(){
+//     $("button").bind("click", function () {
+//         alert(1112);
+//       });
+// })
