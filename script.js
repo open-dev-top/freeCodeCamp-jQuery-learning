c61e5b1d364b54ac6e9b2f26a39ff198568a9970
open dev <opendev.top@gmail.com>
2024-04-17 14:36:56 +0800

    Add method chain

--- script.js
+++ script.js
@@ -7,12 +7,18 @@
 // })
 
 $(function () {
-  //   $("h1").toggle("500");
-  //   $("h1").toggle("normal");
-  $("p").slideToggle("2000");
-  $("p").fadeToggle("2000");
+  $("h1").hide();
+  $("h1").fadeIn(2000).addClass("blanchedalmond");
+
+  $("p").hide();
+  $("p").fadeIn(5000, function () {
+    $(this).addClass("palevioletred");
+  });
+
+  $("img").on("click", function () {
+    console.log("stop");
+    $("p").stop();
+  });
+
+  $("p").hide(500).delay(2000).show(300);
 });
-
-// mouse events: dbclick, mouseenter, mouseleave, mousedown, mouseup, hover
-
-// form events: foucs, blur, change
